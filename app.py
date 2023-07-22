08d196834bb3d0a8ff7e53809e80478fe04fb618
abhijay-agarwal <57358174+abhijay-agarwal@users.noreply.github.com>
2023-07-23 00:28:11 +0800

    test

--- app.py
+++ app.py
@@ -4,9 +4,33 @@ import base64
 
 # Path: app.py
 
+st.set_page_config(
+    page_title="ML for Index Replication",
+    layout="wide",
+)
+
 
 def main():
-    st.title("ML for index replication")
+    cs_body()
+
+    return None
+
+
+def cs_body():
+    # Magic commands
+
+    col1, col2, col3 = st.columns(3)
+
+    col1.subheader("Magic commands")
+    col1.code(
+        """# Magic commands implicitly `st.write()`
+        \'\'\' _This_ is some __Markdown__ \'\'\'
+        a=3
+        'dataframe:', data
+        """
+    )
+
+    return None
 
 
 if __name__ == "__main__":
