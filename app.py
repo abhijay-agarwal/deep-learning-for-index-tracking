8dce95adc71c38eb2575c936218cbd41222021f6
abhijay-agarwal <57358174+abhijay-agarwal@users.noreply.github.com>
2023-07-23 00:25:09 +0800

    getting started

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from pathlib import Path
+import base64
+
+# Path: app.py
+
+
+def main():
+    st.title("ML for index replication")
+
+
+if __name__ == "__main__":
+    main()
